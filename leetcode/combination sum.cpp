b2e25733a3d5cc9fb5716172b35f35bdd5540062
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-04-06 01:09:39 +0900

    Update combination sum.cpp

--- leetcode/combination sum.cpp
+++ leetcode/combination sum.cpp
@@ -1,1 +1,31 @@
 
+class Solution {
+public:
+    void solve(int index, vector<int>& nums, int target, 
+               vector<int>& temp, vector<vector<int>>& result) {
+        
+        // base case
+        if(target == 0) {
+            result.push_back(temp);
+            return;
+        }
+
+        if(index == nums.size() || target < 0) return;
+
+        // ✅ take current element
+        temp.push_back(nums[index]);
+        solve(index, nums, target - nums[index], temp, result); // reuse same element
+        temp.pop_back();
+
+        // ❌ skip current element
+        solve(index + 1, nums, target, temp, result);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> temp;
+
+        solve(0, candidates, target, temp, result);
+        return result;
+    }
+};
